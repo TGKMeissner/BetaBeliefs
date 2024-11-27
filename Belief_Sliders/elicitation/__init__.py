62c5ff795885690557d6e50577d4f026997cd989
TGKMeissner <meissnet@gmail.com>
2024-11-27 21:32:57 +0100

    sliders are there and work

--- Belief_Sliders/elicitation/__init__.py
+++ Belief_Sliders/elicitation/__init__.py
@@ -1,40 +1,35 @@
 from otree.api import *
 
-
-doc = """
-Your app description
-"""
-
-
 class C(BaseConstants):
-    NAME_IN_URL = 'elicitation'
+    NAME_IN_URL = 'beta_distribution'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-
 
 class Subsession(BaseSubsession):
     pass
 
-
 class Group(BaseGroup):
     pass
 
-
 class Player(BasePlayer):
-    pass
+    mean = models.FloatField()
+    variance = models.FloatField()
 
+class Sliders(Page):
+    form_model = 'player'
+    form_fields = ['mean', 'variance']
 
-# PAGES
-class MyPage(Page):
-    pass
-
-
-class ResultsWaitPage(WaitPage):
-    pass
-
+    def vars_for_template(self):
+        min_value = 0  # Replace with your actual min value
+        max_value = 10  # Replace with your actual max value
+        initial_mean = (min_value + max_value) / 2
+        return {
+            'min': min_value,
+            'max': max_value,
+            'initial_mean': initial_mean
+        }
 
 class Results(Page):
     pass
 
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [Sliders, Results]
