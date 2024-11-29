1602c9bc76975b767cffa38b84ede7fab1a6ea2b
TGKMeissner <meissnet@gmail.com>
2024-11-29 12:11:33 +0100

    everything works

--- Belief_Sliders/elicitation/__init__.py
+++ Belief_Sliders/elicitation/__init__.py
@@ -4,6 +4,8 @@ class C(BaseConstants):
     NAME_IN_URL = 'beta_distribution'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
+    MIN_ALLOWED = -100  # Add reasonable bounds for inflation
+    MAX_ALLOWED = 200
 
 class Subsession(BaseSubsession):
     pass
@@ -12,24 +14,44 @@ class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
+    min_inflation = models.FloatField(
+        label='',  # Empty label
+        label_tag=None,  # This prevents label from being rendered
+        min=C.MIN_ALLOWED,
+        max=C.MAX_ALLOWED,
+    )
+    max_inflation = models.FloatField(
+        label='',  # Empty label
+        label_tag=None,  # This prevents label from being rendered
+        min=C.MIN_ALLOWED,
+        max=C.MAX_ALLOWED,
+    )
     mean = models.FloatField()
     variance = models.FloatField()
+
+
+# PAGES
+
+class MinMaxInput(Page):
+    form_model = 'player'
+    form_fields = ['min_inflation', 'max_inflation']
+    
+    def error_message(self, values):
+        if values['min_inflation'] >= values['max_inflation']:
+            return 'The minimum value must be less than the maximum value'
 
 class Sliders(Page):
     form_model = 'player'
     form_fields = ['mean', 'variance']
 
     def vars_for_template(self):
-        min_value = 0  # Replace with your actual min value
-        max_value = 10  # Replace with your actual max value
-        initial_mean = (min_value + max_value) / 2
         return {
-            'min': min_value,
-            'max': max_value,
-            'initial_mean': initial_mean
+            'min': self.min_inflation,
+            'max': self.max_inflation,
+            'initial_mean': (self.min_inflation + self.max_inflation) / 2
         }
 
 class Results(Page):
     pass
 
-page_sequence = [Sliders, Results]
+page_sequence = [MinMaxInput, Sliders, Results]
